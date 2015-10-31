7dba4ea9037bbc9aec8743902c7a1fcdfa3f6dad
Ivan Jurin <ivan.jurin93@gmail.com>
2015-11-01 00:38:18 +0100

    refactor

---
+++ ropaeruj/genetski/src/hr/fer/zemris/optjava/dz4/part2/BoxFilling.java
@@ -0,0 +1,7 @@
+package hr.fer.zemris.optjava.dz4.part2;
+
+/**
+ * Created by ivan on 11/1/15.
+ */
+public class BoxFilling {
+}
--- ropaeruj/genetski/src/hr/fer/zemris/optjava/libopti/utils/Utils.java
+++
@@ -1,7 +0,0 @@
-package hr.fer.zemris.optjava.libopti.utils;
-
-/**
- * Created by ivan on 10/31/15.
- */
-public abstract class Utils {
-}
