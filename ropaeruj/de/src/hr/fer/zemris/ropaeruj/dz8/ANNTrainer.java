1b4e3653d5d907422d570779bb1014ec962cc04b
Ivan Jurin <ivan.jurin93@gmail.com>
2015-12-09 22:27:06 +0100

    add res

---
+++ ropaeruj/de/src/hr/fer/zemris/ropaeruj/dz8/ANNTrainer.java
@@ -0,0 +1,11 @@
+package hr.fer.zemris.ropaeruj.dz8;
+
+/**
+ * Created by ivan on 12/9/15.
+ */
+public class ANNTrainer {
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ ropaeruj/de/test/hr/fer/zemris/ropaeruj/dz8/ANNTrainerTest.java
@@ -0,0 +1,13 @@
+package hr.fer.zemris.ropaeruj.dz8;
+
+import org.junit.Test;
+
+/**
+ * Created by ivan on 12/9/15.
+ */
+public class ANNTrainerTest {
+    @Test
+    public void testMain() throws Exception {
+        ANNTrainer.main(new String[]{""});
+    }
+}
