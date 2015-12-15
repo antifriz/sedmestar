8e5325501528b411625fba022eb7d9f4c66cb649
Ivan Jurin <ivan.jurin93@gmail.com>
2015-12-15 01:49:37 +0100

    added quad test

--- nenr/zad5/test/ann/BackpropAlgTest.java
+++ nenr/zad5/test/ann/BackpropAlgTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class BackpropAlgTest {
 
     @Test
-    public void testRun() throws Exception {
+    public void testQuadratic() throws Exception {
         final List<double[][]> rawDataset = new ArrayList<>();
         for (double i = -1; i <= 1; i += 0.2) {
             rawDataset.add(new double[][]{{i}, {i * i}});
@@ -21,20 +21,14 @@ public class BackpropAlgTest {
 
         IReadOnlyDataset dataset = new BatchReadOnlyDataset(rawDataset,1);
 
-        FFANN ffann = FFANN.create(new int[]{dataset.getInputDimension(), 6,6,6,6,6,6, dataset.getOutputDimension()}, new SigmoidTransferFunction());
+        FFANN ffann = FFANN.create(new int[]{dataset.getInputDimension(), 6, dataset.getOutputDimension()}, new SigmoidTransferFunction());
         BackpropAlg backpropAlg = new BackpropAlg(ffann, dataset);
         double[] weights = backpropAlg.run(0, 1000);
         System.out.println(Arrays.toString(weights));
-
-        System.out.println(Arrays.toString(getOutput(ffann, weights)));
-    }
-
-    private double[] getOutput(FFANN ffann, double[] weights) {
         double[] out = new double[1];
         for (double i = -1; i <= 1; i += 0.1) {
             ffann.calcOutputs(new double[]{i}, weights, out);
             System.out.println(Arrays.toString(out));
         }
-        return out;
     }
 }
