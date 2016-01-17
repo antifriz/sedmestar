50fcc0ef8e6c8362227d3e285b3461af3001c4d5
Ivan Jurin <ivan.jurin93@gmail.com>
2016-01-18 00:30:30 +0100

    changes in nenr repo

--- nenr/zad4/test/hr/fer/zemris/ropaeruj/evo/GeneticAlgorithmTest.java
+++ nenr/zad4/test/hr/fer/zemris/ropaeruj/evo/GeneticAlgorithmTest.java
@@ -2,8 +2,6 @@ package hr.fer.zemris.ropaeruj.evo;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
  * Created by ivan on 10/31/15.
  */
@@ -11,7 +9,7 @@ public class GeneticAlgorithmTest {
 
     @Test
     public void testMain() {
-            GeneticAlgorithm.main("dataset2.txt 100 0 10000 tournament:3 1".split(" "));
+            GeneticAlgorithm.main("dataset2.txt 100 0 10000 rouletteWheel 1".split(" "));
 
     }
 }
